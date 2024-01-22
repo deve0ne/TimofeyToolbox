29d2ef57e59e548c71da80414146fd81298116c5
Deveone <deveoneeee@gmail.com>
2024-01-22 12:59:29 +0300

    version change

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "TimofeyToolbox",
     "author": "Deveone",
-    "version": (1, 4, 1),
+    "version": (1, 4, 2),
     "blender": (3, 5, 1),
     "location": "3D View > Sidebar",
     "wiki_url": "https://github.com/deve0ne/TimofeyToolbox",
